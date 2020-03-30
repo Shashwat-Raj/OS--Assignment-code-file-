b2a464a7be1e1730b760c1c9bf5cf32df904cfd0
Shashwat Raj <62870626+Shashwat-Raj@users.noreply.github.com>
2020-03-30 12:02:16 +0530

    Add files via upload

---
+++ os.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+double page_fault_rate();//declaring a function which would be called for calculations
+void userInput(void);//declaring a function to take user inputs giiven in the problem
+//declaring required entities
+double service_page_fault_empty;
+double service_page_fault_modified;
+double mem_access_time;
+double times_page_modified;
+double effective_access_time;
+double pageFaultRate;
+double service_page_fault_empty_ns;
+double service_page_fault_modified_ns;
+double times_page_modified_per;
+
+void main(){
+	int swtch;
+	do
+	{	
+	printf("Select the required option \n");
+	printf("1.Find the PageFault Rate\n");
+	printf("2.Exit\n");
+	scanf("%d",&swtch);
+	//creating switch for using or exiting the problem calculator
+	switch(swtch){
+		case 1:userInput();break;
+		case 2:exit(0);
+	}
+	printf("\n\n");
+
+}
+while(swtch<3);
+}
+void userInput()
+{	
+	printf("\nEnter service Page Fault [Empty|Page is not Modified][in milliseconds]=");
+	scanf("%lf",&service_page_fault_empty);
+	printf("Enter Service Page Fault [Modified Page][in milliseconds]=");
+	scanf("%lf",&service_page_fault_modified);
+	printf("Enter Memory Access Time[in nanoseconds]=");
+	scanf("%lf",&mem_access_time);
+	printf("Enter Percentage of time the page to be replaced is modified[0-100]=");
+	scanf("%lf",&times_page_modified);
+	printf("Enter Effective Access time[in nanoseconds]=");
+	scanf("%lf",&effective_access_time);
+//input taken through console
+/*Formula:         EAT = (1-p)*(mac) + (p)*(100 + (1-r)*(spfe) + (r)*(spf))   
+where EAT=Effective Access Time, p=page fault ,r=(percentage of time)/100,  
+spfe=service page fault(not modified), spf=service page fault(modified), mac=memory access time.
+*/
+
+	service_page_fault_empty_ns = (service_page_fault_empty*1000000);//converted into absolute value
+	service_page_fault_modified_ns = (service_page_fault_modified*1000000);//converted into absolute value
+	times_page_modified_per = (times_page_modified/100);   
+	printf("\nPage Fault rate calculated For:\n");
+	printf("Service Page Fault[Empty|Page Not Modified]=%lf \n",service_page_fault_empty_ns);
+	printf("Service Page Fault [Modified Page][in nanoseconds]= %lf \n",service_page_fault_modified_ns);
+	printf("Memory Access Time[in nanoseconds]=%lf\n",mem_access_time);
+	printf("Effective Access Time=%lf\n",effective_access_time);
+    pageFaultRate =  page_fault_rate(service_page_fault_empty_ns,service_page_fault_modified_ns,mem_access_time,times_page_modified_per,effective_access_time);
+	printf("\nMaximum Acceptable Page Fault rate = %.2e[exponential notation]",pageFaultRate);
+}
+double page_fault_rate(double servicePageFaultEmpty,double servicePageFaultMod,double memAccess,double timesPages,double effAccess){
+	double assume,serve;
+	double numerator,denominator;
+	double pageFault;
+	 assume = (1- timesPages)*servicePageFaultEmpty;
+	 serve = timesPages*servicePageFaultMod;
+	 numerator = effAccess - memAccess;
+     denominator = (assume+serve);
+
+	pageFault = numerator/denominator;
+	return pageFault;
+}
